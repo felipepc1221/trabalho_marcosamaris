3066974fcc39b23025322ecd44dec9c4e683ad16
felipepc1221 <121831334+felipepc1221@users.noreply.github.com>
2024-04-09 14:31:29 -0300

    Add files via upload

---
+++ hello_world.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+int imprime(){
+    printf("Hello World!\n");
+    return 0;
+}
+char 
+int main
